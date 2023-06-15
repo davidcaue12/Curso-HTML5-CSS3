2aef506cb35ef69353330bd814567f8f823b93d0
davidcaue12 <davidcaue8@gmail.com>
2023-06-15 01:00:56 -0300

    ;

---
+++ praticar/ex08-treinando_mobile_first/assets/Js/index.js
@@ -0,0 +1,9 @@
+function mobile(){
+    document.querySelector('.mobile').style.width = '50%'
+    document.querySelector('.fa-bars').style.display = 'none'
+}
+
+function mobile_close(){
+    document.querySelector('.mobile').style.width = '0%'
+    document.querySelector('.fa-bars').style.display = 'block'
+}
