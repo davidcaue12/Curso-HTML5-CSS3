f5f72133e2609debf65789bf7e4270cd61e01d8c
davidcaue12 <davidcaue8@gmail.com>
2023-08-17 20:42:27 -0300

    Estudos na alura JavaScript; Estudos Guanabara HTML CSS;

---
+++ Praticas JS/index.js
@@ -0,0 +1,10 @@
+const ator = document.getElementById('ator')
+const carro = document.getElementById('carro')
+const background = document.getElementById('background')
+
+function mover(){
+    carro.style.right = '5px'
+}
+
+mover()
+
